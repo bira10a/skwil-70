be910c008e16835d8d11411add3542731833ddcd
Rodchenko Nikita <bira10a@gmail.com>
2022-02-14 22:38:52 +1000

    почти дз 13 только штрихи

--- app/js/main.js
+++ app/js/main.js
@@ -6,12 +6,26 @@ $(function(){
   })
 
   $('.top-slider__items').slick({
-    prevArrow: '<button type="button" class="slick-prev"><img src="images/arrow-prev.svg" alt=""></button>',
-    nextArrow: '<button type="button" class="slick-next"><img src="images/arrow-next.svg" alt=""></button>',
+    prevArrow: '<button type="button" class="slick-prev"></button>',
+    nextArrow: '<button type="button" class="slick-next"></button>',
     fade: true,
     autoplay: true,
     autoplaySpead: 2000,
   })
 
-  var mixer = mixitup('.top-products__content');
+  // var mixer = mixitup('.top-products__content'); для одного слайдера на странице, тогда div container не нужен
+
+  var containerEl1 = document.querySelector('[data-ref="container-1"]');
+  var containerEl2 = document.querySelector('[data-ref="container-2"]');
+
+  var config = {
+    controls: {
+      scope: 'local'
+    }
+  };
+
+  var mixer1 = mixitup(containerEl1, config);
+  var mixer2 = mixitup(containerEl2, config);
+
+  
 })
